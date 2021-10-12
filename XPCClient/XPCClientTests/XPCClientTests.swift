3858f63fddc260a5721a454c25a642dd6ae3ed07
uweiyuan <uweiyuan@tencent.com>
2021-10-12 16:57:50 +0800

    update UT

--- XPCClient/XPCClientTests/XPCClientTests.swift
+++ XPCClient/XPCClientTests/XPCClientTests.swift
@@ -18,20 +18,8 @@ class XPCClientTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
     
-    func testLogic() {
+    func testLogic() throws {
         if #available(iOS 11, *) {
             XPCManager.default().listener(with: XPCClientProxy())
         } else {
